7070282fb4c04a78321e56c7649364fda586856d
alenushka <alionxlo@gmail.com>
2022-11-30 20:14:16 +0300

    Pushes the data aside

--- src/components/NewsFeed/NewPost/NewPost.jsx
+++ src/components/NewsFeed/NewPost/NewPost.jsx
@@ -7,15 +7,17 @@ import iconKebab from '../../../icon/icon-kebab.svg';
 const NewPost = () => {
   return (
     <div className='newPost'>
-      <img src="https://avatarko.ru/img/kartinka/3/zhivotnye_sobaka_2456.jpg" weight='50' height='50' alt=""></img>
-      <div className='newPost__data-user'>
-        <span>Sepural Gallery</span>
-        <span>15h, Public</span>
+      <div className='newPost__top'>
+        <img src="https://avatarko.ru/img/kartinka/3/zhivotnye_sobaka_2456.jpg" weight='50' height='50' alt=""></img>
+        <div className='newPost__data-user'>
+          <span>Sepural Gallery</span>
+          <span>15h, Public</span>
+        </div>
+        <button className='newPost__settings' type='button'>
+          <HandySvg src={iconKebab} width="20" height="20" />
+        </button>
       </div>
-      <button className='newPost__settings' type='button'>
-        <HandySvg src={iconKebab} width="20" height="20" />
-        <PostSettings />
-      </button>
+      <PostSettings />
       <img className='newPost__picture' src="https://assets.weforum.org/article/image/responsive_large_webp_6llmOwHcapGd0p2ilOGJBwKBQI8mnQfVmcI8Xi5F8aA.webp" weight='660' height='347' alt=""></img>
     </div>
   )
--- src/components/NewsFeed/NewPost/PostSettings/PostSettings.jsx
+++ src/components/NewsFeed/NewPost/PostSettings/PostSettings.jsx
@@ -6,31 +6,44 @@ import iconNotification from '../../../../icon/icon-notification.svg';
 import iconAttention from '../../../../icon/icon-attention.svg';
 import iconUnfollow from '../../../../icon/icon-user-unfollow.svg';
 
+const SettingsItem = (props) => {
+  return (
+    <span className='postSettings__text'>{props.text}</span>
+  )
+}
+
 const PostSettings = () => {
+  let settingText = [
+    { id: 1, text: 'Hide Post' },
+    { id: 1, text: 'Turn on notification for this post' },
+    { id: 1, text: 'Report this post' },
+    { id: 1, text: 'Unfollow' },
+  ]
+
   return (
     <ul className='postSettings'>
       <li>
         <button type='button'>
           <HandySvg src={iconHide} width="20" height="20" />
-          <span className='postSettings__text'>Hide Post</span>
+          <SettingsItem text={settingText[0].text} />
         </button>
       </li>
       <li>
         <button type='button'>
           <HandySvg src={iconNotification} width="20" height="20" />
-          <span className='postSettings__text'>Turn on notification for this post</span>
+          <SettingsItem text={settingText[1].text} />
         </button>
       </li>
       <li>
         <button type='button'>
           <HandySvg src={iconAttention} width="20" height="20" />
-          <span className='postSettings__text'>Report this post</span>
+          <SettingsItem text={settingText[2].text} />
         </button>
       </li>
       <li>
         <button type='button'>
           <HandySvg src={iconUnfollow} width="20" height="20" />
-          <span className='postSettings__text'>Unfollow</span>
+          <SettingsItem text={settingText[3].text} />
         </button>
       </li>
     </ul>
