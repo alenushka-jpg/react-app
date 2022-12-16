0dac3bee4e2bf27e68cc56a587db47f99227fa7f
alenushka <alionxlo@gmail.com>
2022-12-16 19:43:19 +0300

    Adds a MyFriends component

--- src/components/NewsFeed/NewsFeed.jsx
+++ src/components/NewsFeed/NewsFeed.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import './NewsFeed.css';
 import News from './News/News';
 import NewPosts from './NewPost/NewPost';
+import MyFriends from '../MyFriends/MyFriends'
 
 const NewsFeed = () => {
   return (
     <section className='newsFeed'>
       <News />
       <NewPosts />
+      <MyFriends />
     </section>
   )
 }
